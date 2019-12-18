865a5f31fb1ac1f4f9c753bd1cc76fbc5c980f2c
Maksim Gorokhovskiy <magor1744@gmail.com>
2019-12-18 17:16:37 +0300

    remove build folder

--- Build/Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/magor/OneDrive/Documents/hse/Projects/Project 2/PathPlanningProject
-# Build directory: C:/Users/magor/OneDrive/Documents/hse/Projects/Project 2/PathPlanningProject/Build/Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Src")
-subdirs("Tests")
--- Build/Debug/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/magor/OneDrive/Documents/hse/Projects/Project 2/PathPlanningProject/Src
-# Build directory: C:/Users/magor/OneDrive/Documents/hse/Projects/Project 2/PathPlanningProject/Build/Debug/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
